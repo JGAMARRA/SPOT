d3f893891759b2f324b6a7784d3ab366d849d2fa
Lester León <lester77rlv@gmail.com>
2018-07-29 21:37:11 -0500

    Datatable

---
+++ SPOT.Entity/ProveedorBE.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SPOT.Entity
+{
+    public class ProveedorBE
+    {
+        public int IdProveedor { get; set; }
+        public string RUC { get; set; }
+        public string Proveedor { get; set; }
+        public string Contacto { get; set; }
+        public string Direccion { get; set; }
+    }
+}
--- SPOT.Web/Controllers/HomeController.cs
+++ SPOT.Web/Controllers/HomeController.cs
@@ -4,7 +4,10 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SPOT.Data;
+using SPOT.Entity;
 using SPOT.Web.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace SPOT.Web.Controllers
 {
@@ -13,6 +16,57 @@ namespace SPOT.Web.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult GetAll()
+        {
+            int draw = 1; //Convert.ToInt32(request["draw"]);
+            int start = 0; //Convert.ToInt32(Request["start"]);
+            int length = 10; // Convert.ToInt32(Request["length"]);
+            int orderColumn = 0; //Convert.ToInt32(Request["order[0][column]"]);
+            string orderDir = "asc"; //Request["order[0][dir]"];
+            string searchValue = ""; // Request["search[value]"];
+
+            List<ProveedorBE> list = new ProveedorDA().GetAll();
+
+            int listCount = list.Count();
+
+            if (!string.IsNullOrEmpty(searchValue))
+            {
+                list = list.
+                    Where(x =>
+                    x.IdProveedor.ToString().ToLower().Contains(searchValue.ToLower()) ||
+                    x.RUC.ToLower().Contains(searchValue.ToLower()) ||
+                    x.Proveedor.ToLower().Contains(searchValue.ToLower()) ||
+                    x.Contacto.ToLower().Contains(searchValue.ToLower()) ||
+                    x.Direccion.ToString().Contains(searchValue.ToLower())
+                ).ToList();
+            }
+
+            int filterCount = list.Count;
+
+            switch (orderColumn)
+            {
+                case 0: //IdProveedor
+                    list = orderDir == "asc" ? list.OrderBy(x => x.IdProveedor).ToList() : list.OrderByDescending(x => x.IdProveedor).ToList();
+                    break;
+                case 1: //RUC
+                    list = orderDir == "asc" ? list.OrderBy(x => x.RUC).ToList() : list.OrderByDescending(x => x.RUC).ToList();
+                    break;
+                case 2: //Proveedor
+                    list = orderDir == "asc" ? list.OrderBy(x => x.Proveedor).ToList() : list.OrderByDescending(x => x.Proveedor).ToList();
+                    break;
+                case 3: //Contacto
+                    list = orderDir == "asc" ? list.OrderBy(x => x.Contacto).ToList() : list.OrderByDescending(x => x.Contacto).ToList();
+                    break;
+                case 4: //Direccion
+                    list = orderDir == "asc" ? list.OrderBy(x => x.Direccion).ToList() : list.OrderByDescending(x => x.Direccion).ToList();
+                    break;
+            }
+
+            list = list.Skip(start).Take(length).ToList();
+
+            return Json(new { data = list, draw = draw, recordsTotal = listCount, recordsFiltered = filterCount });
         }
 
         public IActionResult About()
